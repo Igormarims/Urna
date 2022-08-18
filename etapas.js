24e9255604bce038f3be45a15d7e90163e014b0d
Igor marques <igormarquespe@gmail.com>
2022-08-18 15:33:08 -0300

    Adicionando JavaScript ao Projeto

---
+++ etapas.js
@@ -0,0 +1,54 @@
+  
+  let etapas = [
+      {
+      titulo: 'VERIADOR',
+      numero: 5,
+      candidatos: [
+          {
+              numero: 38111,
+              name: 'fulano de tal',
+              partido: 'ABC',
+              fotos:[
+              {url: '38111.jpg', legenda: 'veriador', snall: false}
+              ]
+          },
+          {
+            numero : 77222,
+            name: 'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos: [
+                {url: '772222.jpg', legenda: 'Vereador'}
+            ]
+          },
+        
+        ]
+      },
+          {
+            titulo: 'PREFEITO',
+            numero: 99,
+            candidatos:[
+              {
+            name: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos: [
+            {url: '99.jpg', legenda: 'perfeito'},
+            {url: '99_2.jpg', leguenda: 'Vice-prefeito', small: false}
+            ]
+          },
+          {
+            numero: 84,
+            name: 'Zulano',
+            partido: 'QWERTY',
+            vice: 'Zul',
+            fotos: [
+              {url: '84.jpg', leguenda: 'Prefeito'},
+              {url: '84.2.jpg', leguenda: 'Vice-prefeito'}
+            ]
+          },
+        ]
+      }
+    ]
+
+
+
